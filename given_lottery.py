edcbe6b2a6016357e1d458cad707c3e2d1c64c7c
tasou-pt <tasou@fab.pasona.tech>
2020-07-05 19:50:23 +0900

    乱数生成せずにchoice関数で直接選ぶ

--- given_lottery.py
+++ given_lottery.py
@@ -5,10 +5,7 @@ def draw_lots(data=None):
     if data is None:
         return -1
     
-    lot_member = data.split(',')
-    r = secrets.randbelow(len(lot_member))
-
-    return lot_member[r]
+    return secrets.choice(data.split(','))
 
 if __name__ == '__main__':
     args = sys.argv
